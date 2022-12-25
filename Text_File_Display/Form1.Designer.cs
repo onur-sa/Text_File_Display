2ed4d2fff4ce2303b325a0172ff6e1071be26b7c
onurs <onurs@DESKTOP-8IG6IRC>
2022-12-25 20:05:40 +0300

    Add project files.

---
+++ Text_File_Display/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Text_File_Display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 79);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(59, 116);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(587, 240);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "File Path : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "File Extension :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 391);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Text_File_Display/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+
+// Difference between list and array
+//
+
+
+namespace Text_File_Display
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] lines;
+        
+        List<string> mylist = new List<string>();
+        string file_path = "";
+        string file_extension = "";
+        int lineCount = 0;
+
+        void ReadTextFile(string file_path, List<string> mylist)
+        {
+
+            // .CompareTo() ifadesi file_extension ile ".txt" stringini karşılaştırır. Eğer aynı ise int 0 dönüdürür. 
+            if (file_extension.CompareTo(".txt") == 0)  // yani dosya uzantıları aynı ise --->
+            {
+
+                try
+                {
+                    StreamReader reader = new StreamReader(file_path);
+
+                    while (!reader.EndOfStream)
+                    {
+
+                        mylist.Add(reader.ReadLine());
+                        lineCount++;
+
+                    }
+                    reader.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+                //label5.Text = lineCount.ToString();
+            }
+        }
+
+        void DisplayTextFileOnTextBox(String[] lines, RichTextBox richTextBox1)
+        {
+            for (int i = 0; i < lineCount; i++)
+            {
+                richTextBox1.Text += lines[i] + "\n";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Dosya browse etmek için açılan pencereye "OpenFileDialog penceresi" deniyor.
+            // Burada openFileDialog adında bir nesne oluşturup nesneye dair özellikler ekledim.
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = " Open File ";
+            openFileDialog.Filter = "txt_file (*.txt)|";
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)  // Burada ise openFileDialog dialog ekranı gösterildiğinde(.ShowDialog())
+            {                                                   // birşey seçilmişse şunları yap şeklinde bir if yazdım.            
+                file_path = openFileDialog.FileName;     // file_path adındaki boş stringe seçtiğim dosyanın dosya yolunu ekledim. 
+                                                         // bu dosya yoluna .FileName' i kullandığım için dosyanın adını da eklemiş oldum.                                                                          
+                label1.Text = file_path;
+                
+                file_extension = Path.GetExtension(file_path); // Daha sonra Path.GetExtension() metodu ile file_path' deki dosyanın uzantısını aldım.     
+                
+                label2.Text = file_extension;
+
+
+                //// var lineCount = File.ReadLines(file_path).Count();
+
+                ReadTextFile(file_path, mylist);
+                String[] lines = mylist.ToArray();
+
+                DisplayTextFileOnTextBox(lines, richTextBox1);
+
+            }
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
